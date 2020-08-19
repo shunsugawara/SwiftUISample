6760d195221116b4a1cfd269e55afc5eaad62a52
Shun Sugawara <shn.sgwr@gmail.com>
2020-08-19 21:32:46 +0900

    create project

---
+++ SwiftUISamples/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUISamples
+//
+//  Created by 菅原　駿 on 2020/08/19.
+//  Copyright © 2020 菅原　駿. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
